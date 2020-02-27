0341bbed3d802334787fbedcac601828e84fa2c7
Rowan-Paul <46132597+Rowan-Paul@users.noreply.github.com>
2020-02-27 18:36:05 +0100

    Added read more about me button
    Added:
     - read more about me button

--- index.php
+++ index.php
@@ -3,9 +3,13 @@ $title = "Homepage";
 include_once 'standard-pages/header.php';
 ?>
 
-<main class="flex-center">
+<main class="flex-center" id="background">
   <h1 class="title">Welcome</h1>
-  <p class="sub-title">On this site you'll be able to read about me, my projects and other cool stuff related to me.</p>
+  <p class="sub-title">My name is Rowan Paul Flynn and on this site you'll be able to read about me, my projects and other cool stuff related to me.</p>
+
+  <div class="button">
+    <a href="about.php">Read more about me</a>
+  </div>
 </main>
 
 <?php
--- standard-pages/header.php
+++ standard-pages/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $title ?> | Rowan-Paul Flynn</title>
+    <title><?php echo $title ?> | Rowan Paul Flynn</title>
 
     <link rel="stylesheet" type="text/css" href="assets/style.css">
     <script src="assets/main.js"></script>
