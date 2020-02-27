91cb091c3af793f8df8d2903049ec52baf42050b
Rowan-Paul <46132597+Rowan-Paul@users.noreply.github.com>
2020-02-27 17:25:43 +0100

    Styled welcome message
    Added:
     - styling to welcome message per title & sub-title class

--- index.php
+++ index.php
@@ -3,9 +3,9 @@ $title = "Homepage";
 include_once 'standard-pages/header.php';
 ?>
 
-<main>
-  <h1>Welcome to my site</h1>
-  <p>On this site you'll be able to read about me, my projects and other cool stuff related to me.</p>
+<main class="flex-center">
+  <h1 class="title">Welcome</h1>
+  <p class="sub-title">On this site you'll be able to read about me, my projects and other cool stuff related to me.</p>
 </main>
 
 <?php
