2c6026d8b56ab5a04095352432e708109efc2b1c
Rowan-Paul <46132597+Rowan-Paul@users.noreply.github.com>
2020-03-19 18:21:42 +0100

    Added page names
    Added:
     - page header and description into the blog and contact pages

--- blog.php
+++ blog.php
@@ -4,7 +4,8 @@ include_once 'standard-pages/header.php';
 ?>
 
 <main>
-  
+    <h2>Blog</h2>
+    <p>On this page you can read my blog where I post what's happening with my life, projects and just other stuff I like to post such as reviews.</p>
 </main>
 
 <?php
--- contact.php
+++ contact.php
@@ -4,7 +4,8 @@ include_once 'standard-pages/header.php';
 ?>
 
 <main>
-  
+    <h1>Contact</h1>
+    <p>If you want to contact me, you can do that here.</p>
 </main>
 
 <?php
