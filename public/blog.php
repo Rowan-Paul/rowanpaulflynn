c4f9d5d613bee877274c62bcd7fa21f11e470b6e
Rowan-Paul <rpflynn80@gmail.com>
2020-04-29 13:39:32 +0200

    REMOVED page under construction

--- public/blog.php
+++ public/blog.php
@@ -6,8 +6,6 @@ include_once 'includes/header.php';
 <main>
     <h1>Blog</h1>
     <p>On this page you can read my blog where I post what's happening with my life, projects and just other stuff I like to post such as reviews.</p>
-
-    <h2>This page is still under construction</h2>
 </main>
 
 <?php
--- public/contact.php
+++ public/contact.php
@@ -6,8 +6,6 @@ include_once 'includes/header.php';
 <main>
     <h1>Contact</h1>
     <p>If you want to contact me, you can do that here.</p>
-
-    <h2>This page is still under construction</h2>
 </main>
 
 <?php
