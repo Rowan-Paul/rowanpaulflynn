6765b0961e3db5886d46d5ea171a9930293f5dc5
Rowan-Paul <rpflynn80@gmail.com>
2020-04-29 12:49:51 +0200

    ADD page under construction to contact & blog

--- blog.php
+++ blog.php
@@ -6,6 +6,8 @@ include_once 'includes/header.php';
 <main>
     <h2>Blog</h2>
     <p>On this page you can read my blog where I post what's happening with my life, projects and just other stuff I like to post such as reviews.</p>
+
+    <h2>This page is still under construction</h2>
 </main>
 
 <?php
