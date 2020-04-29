dda16b556acdc4222bbd90bf2fa8ac2ad5a09cdc
Rowan-Paul <rpflynn80@gmail.com>
2020-04-29 13:28:21 +0200

    UPDATE blog title to h1

--- public/blog.php
+++ public/blog.php
@@ -4,7 +4,7 @@ include_once 'includes/header.php';
 ?>
 
 <main>
-    <h2>Blog</h2>
+    <h1>Blog</h1>
     <p>On this page you can read my blog where I post what's happening with my life, projects and just other stuff I like to post such as reviews.</p>
 
     <h2>This page is still under construction</h2>
