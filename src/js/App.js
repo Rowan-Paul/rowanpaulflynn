39e0157605a2fd5292b6655d5a3b05ddd54fc874
Rowan-Paul <rpflynn80@gmail.com>
2021-01-28 12:32:47 +0100

    Add homepage

---
+++ src/js/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="introduction">
+      <h1>Hello there</h1>
+      <h2>My name is Rowan Paul Flynn</h2>
+      <h3>
+        I like making <span className="emphasis">websites</span>
+      </h3>
+    </div>
+  );
+}
+
+export default App;
