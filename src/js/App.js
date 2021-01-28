22b06bf1177c764ca2e155c47597793a3e6bc453
Rowan-Paul <rpflynn80@gmail.com>
2021-01-28 18:36:38 +0100

    Add system mode check

--- src/js/App.js
+++ src/js/App.js
@@ -1,8 +1,25 @@
+import { useEffect } from "react";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 
 import { LandingPage } from "./landing-page/LandingPage";
 
 function App() {
+  useEffect(() => {
+    if (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches &&
+      localStorage.theme !== "light"
+    ) {
+      localStorage.theme = "dark";
+    }
+
+    if (localStorage.theme === "dark") {
+      document.querySelector("html").classList.add("dark");
+    } else {
+      document.querySelector("html").classList.remove("dark");
+    }
+  }, []);
+
   return (
     <Router>
       <Switch>
