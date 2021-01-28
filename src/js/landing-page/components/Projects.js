39cac0035a14c6e7a5bcd418d36b9d6f2c9d25a0
Rowan-Paul <rpflynn80@gmail.com>
2021-01-28 14:50:25 +0100

    Open github link in new tab

--- src/js/landing-page/components/Projects.js
+++ src/js/landing-page/components/Projects.js
@@ -7,7 +7,14 @@ function ProjectsUI() {
       <p>
         Here are a few web projects I''m really proud of. The code for all these
         projects are available on my{" "}
-        <a href="https://github.com/Rowan-Paul/">GitHub page</a>.
+        <a
+          href="https://github.com/Rowan-Paul/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub page
+        </a>
+        .
       </p>
     </div>
   );
