010076287b1a0d8c3806a05b20db81ec6ef4c65a
Rowan-Paul <rpflynn80@gmail.com>
2021-01-28 14:02:29 +0100

    Add introduction taking up entire screen

--- src/js/App.js
+++ src/js/App.js
@@ -4,11 +4,13 @@ function App() {
   return (
     <Fragment>
       <div className="introduction">
-        <h1>Hello there</h1>
-        <h2>My name is Rowan Paul Flynn</h2>
-        <h3>
-          And I like making <span className="emphasis">websites</span>
-        </h3>
+        <div className="introductionText">
+          <h1>Hello there</h1>
+          <h2>My name is Rowan Paul Flynn</h2>
+          <h3>
+            And I like making <span className="emphasis">websites</span>
+          </h3>
+        </div>
       </div>
 
       <div className="bio">
