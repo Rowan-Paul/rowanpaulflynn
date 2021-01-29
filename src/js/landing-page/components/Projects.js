45aa6f19a89dea66458feb258f512bfdb2c51975
Rowan-Paul <rpflynn80@gmail.com>
2021-01-29 12:08:37 +0100

    Fix extra '

--- src/js/landing-page/components/Projects.js
+++ src/js/landing-page/components/Projects.js
@@ -5,7 +5,7 @@ function ProjectsUI() {
         <span className="header">Projects</span>
       </h2>
       <p>
-        Here are a few web projects I''m really proud of. The code for all these
+        Here are a few web projects I'm really proud of. The code for all these
         projects are available on my{" "}
         <a
           href="https://github.com/Rowan-Paul/"
