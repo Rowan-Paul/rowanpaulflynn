f5d2c473314fde7f4f948f11bc660dbc7d243eaa
Rowan-Paul <rpflynn80@gmail.com>
2021-02-08 16:25:32 +0100

    Update project images

--- src/js/landing-page/components/Projects.js
+++ src/js/landing-page/components/Projects.js
@@ -21,39 +21,57 @@ function ProjectsUI() {
         .
       </p>
 
-      <h3>Rekenlogboek (school project)</h3>
-      <img src={rekenlogboek} alt="Screenshot of rekenlogboek" />
-      <p>
-        The rekenlogboek app was made together with 4 others. The idea was to
-        digitalize the so called Rekenlogboek for primary school children. The
-        project utilised React, Express, MongoDB, Socket.io, Microsoft Identity
-        Framework and ran (partly) inside Microsoft Teams. The server has unit
-        tests with Jest and the client has end-to-end tests with Puppeteer.
-      </p>
-      <p>
-        During this project I learned a lot about authentication using the
-        Microsoft Identity Framework and the very basics of testing with Jest
-        and Puppeteer.
-      </p>
+      <div className="projectContainer">
+        <h3 className="projectTitle">Rekenlogboek (school project)</h3>
+        <img
+          src={rekenlogboek}
+          alt="Screenshot of rekenlogboek"
+          className="projectThumb"
+        />
+        <p>
+          The rekenlogboek app was made together with 4 others. The idea was to
+          digitalize the so called Rekenlogboek for primary school children. The
+          project utilised React, Express, MongoDB, Socket.io, Microsoft
+          Identity Framework and ran (partly) inside Microsoft Teams. The server
+          has unit tests with Jest and the client has end-to-end tests with
+          Puppeteer.
+        </p>
+        <p>
+          During this project I learned a lot about authentication using the
+          Microsoft Identity Framework and the very basics of testing with Jest
+          and Puppeteer.
+        </p>
+      </div>
 
-      <h3>Blog</h3>
-      <img src={blog} alt="Screenshot of blog" />
-      <p>
-        I have a personal blog where I post things occasionally. It's made with
-        Gatsby, a framework to run React on the client side. The site is
-        deployed using CI with GitHub Pages.
-      </p>
+      <div className="projectContainer">
+        <h3 className="projectTitle">Blog</h3>
+        <img src={blog} alt="Screenshot of blog" className="projectThumb" />
+        <p>
+          I have a personal blog where I post things occasionally. It's made
+          with Gatsby, a framework to run React on the client side. The site is
+          deployed using CI with GitHub Pages.
+        </p>
+      </div>
 
-      <h3>Quizzer (school project</h3>
-      <img src={quizzer} alt="Screenshot of quizzer" />
-      <p>
-        Quizzer was made together with another student during the DWA course.
-        The assisgnement was to create a pub quiz application. It was made out
-        of 4 different apps: three for the client-side and one for the
-        server-side. The project utilised React, Express and WebSockets. During
-        this project I learned a lot about the connection between server and
-        client, such as CORS and when to use WebSockets.
-      </p>
+      <div className="projectContainer">
+        <h3 className="projectTitle">Quizzer (school project)</h3>
+        <img
+          src={quizzer}
+          alt="Screenshot of quizzer"
+          className="projectThumb"
+        />
+        <p>
+          Quizzer was made together with another student during the DWA course.
+          The assisgnement was to create a pub quiz application. It was made out
+          of 4 different apps: three for the client-side and one for the
+          server-side.
+        </p>
+        <p>
+          The project utilised React, Express and WebSockets. During this
+          project I learned a lot about the connection between server and
+          client, such as CORS and when to use WebSockets.
+        </p>
+      </div>
     </div>
   );
 }
