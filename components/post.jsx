1c0cd6cd0efb8e6ad9c38e4bd37604aa5e2d01e1
Rowan-Paul <contact@rowanpaulflynn.com>
2021-08-29 20:12:48 +0200

    Add only show intro on Post component

--- components/post.jsx
+++ components/post.jsx
@@ -4,6 +4,7 @@ import ReactMarkdown from 'react-markdown'
 
 const Post = ({ post }) => {
   const authorName = post.author ? post.author.name : 'Unknown author'
+
   return (
     <div
       onClick={() => Router.push('/blog/posts/[id]', `/blog/posts/${post.id}`)}
@@ -11,7 +12,7 @@ const Post = ({ post }) => {
     >
       <h2>{post.title}</h2>
       <small>By {authorName}</small>
-      <ReactMarkdown children={post.content} />
+      <ReactMarkdown children={post.content?.split('\n')[0]} />
     </div>
   )
 }
