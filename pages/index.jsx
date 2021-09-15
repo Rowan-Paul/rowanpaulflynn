5e3d06f110b57ec56404804355a3603b41f42d8a
Rowan-Paul <contact@rowanpaulflynn.com>
2021-09-15 10:52:54 +0200

    Fix images on homepage

--- pages/index.jsx
+++ pages/index.jsx
@@ -28,12 +28,10 @@ export default function Home() {
             <span className="p-2 md:ml-1 underline">About me</span>
           </h2>
           <p>
-            I live in the Netherlands and am currently studying web development.
-            In my free time I like programming cool things, playing games,
-            watching stuff on my phone and reading the occasional book. My
-            preferred programming language is JavaScript, especially React but
-            I've had experience with quite a few different languages. You can
-            see the languages I've worked with down below.
+            I live in the Netherlands and am currently studying web development. In my free time I like programming cool
+            things, playing games, watching stuff on my phone and reading the occasional book. My preferred programming
+            language is JavaScript, especially React but I've had experience with quite a few different languages. You
+            can see the languages I've worked with down below.
           </p>
 
           <Languages />
@@ -44,13 +42,8 @@ export default function Home() {
             <span className="p-2 md:ml-1 underline">Projects</span>
           </h2>
           <p>
-            Here are a few projects of mine that you might find interesting. You
-            can find more projects on my{' '}
-            <a
-              href="https://github.com/Rowan-Paul/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            Here are a few projects of mine that you might find interesting. You can find more projects on my{' '}
+            <a href="https://github.com/Rowan-Paul/" target="_blank" rel="noopener noreferrer">
               GitHub page
             </a>
             .
@@ -59,36 +52,31 @@ export default function Home() {
           <div className="m-0 md:m-20 overflow-auto">
             <h3 className="underline">Rekenlogboek (school project)</h3>
             <img
-              src="https://raw.githubusercontent.com/Rowan-Paul/rowanpaulflynn/main/src/assets/projects/rekenlogboek.jpeg"
+              src="/rekenlogboek.jpeg"
               alt="Screenshot of rekenlogboek"
               className="block mb-4 max-w-100 md:float-left md:mr-4 md:max-w-1/2"
             />
             <p>
-              The rekenlogboek app was made together with 4 others during a
-              school project. The rekenlogboek allows primary school teachers to
-              ask their students how they did during tests and explanations.
+              The rekenlogboek app was made together with 4 others during a school project. The rekenlogboek allows
+              primary school teachers to ask their students how they did during tests and explanations.
             </p>
             <p>
-              By converting it to a digital application teachers could get a
-              quicker overview of the performance of the class. The project used
-              React, Redux, Node, Microsoft Identity Framework, Socket.io,
-              Puppeteer &amp; Jest.
+              By converting it to a digital application teachers could get a quicker overview of the performance of the
+              class. The project used React, Redux, Node, Microsoft Identity Framework, Socket.io, Puppeteer &amp; Jest.
             </p>
           </div>
 
           <div className="m-0 md:m-20 overflow-auto">
             <h3 className="underline">GWENTcards</h3>
             <img
-              src="https://raw.githubusercontent.com/Rowan-Paul/rowanpaulflynn/main/src/assets/projects/gwentcards.jpg"
+              src="/gwentcards.jpg"
               alt="Screenshot of GWENTcards"
               className="block mb-4 max-w-100 md:float-left md:mr-4 md:max-w-1/2"
             />
             <p>
-              GWENTcards is a site build with React &amp; Node that allows users
-              to keep track of what cards they have collected for The Witcher 3:
-              Wild Hunt card mini game GWENT. It is also a PWA with full offline
-              functioniality and is currently deployed on{' '}
-              <a href="https://gwentcards.net/">gwentcards.net</a>.
+              GWENTcards is a site build with React &amp; Node that allows users to keep track of what cards they have
+              collected for The Witcher 3: Wild Hunt card mini game GWENT. It is also a PWA with full offline
+              functioniality and is currently deployed on <a href="https://gwentcards.net/">gwentcards.net</a>.
             </p>
           </div>
         </div>
