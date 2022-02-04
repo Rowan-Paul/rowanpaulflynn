c29cfd7e3a338c9cdfd6593bfd5c0980e4592ff8
Rowan-Paul <contact@rowanpaulflynn.com>
2022-02-04 09:15:50 +0100

    Remove projects, fix mobile view

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,22 @@ const Home: NextPage = () => {
   return (
     <>
       <Section>
-        <div className="flex align-middle justify-center h-screen items-center">
+        <div className="flex align-middle justify-center h-screen -mt-10 md:mt-0 items-center">
           <div className="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-orange-400 to-purple-600">
-            <span className="text-xl md:text-3xl">Hi, my name is</span>
-            <h1 className="text-5xl md:text-8xl mt-2 mb-4">Rowan Paul Flynn</h1>
-            <span className="text-2xl md:text-5xl">and I am a web developer</span>
+            <span className="text-l md:text-3xl">Hi, my name is</span>
+            <h1 className="text-3xl md:text-8xl mt-2 mb-4">Rowan Paul Flynn</h1>
+            <span className="text-xl md:text-5xl">and I am a web developer</span>
           </div>
         </div>
       </Section>
+      <Link href="#socials">
+        <a className="absolute bottom-0 left-0 right-0 ml-auto mr-auto w-12 text-xs md:text-base text-white cursor-pointer hover:animate-bounce mb-5">
+          <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 0 24 24" width="48px" fill="#FFFFFF">
+            <path d="M24 24H0V0h24v24z" fill="none" opacity=".87" />
+            <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6-1.41-1.41z" />
+          </svg>
+        </a>
+      </Link>
       <Section>
         <div
           id="socials"
@@ -108,73 +116,12 @@ const Home: NextPage = () => {
           </div>
         </div>
       </Section>
-      <Section>
-        <div
-          id="projects"
-          className="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 to-purple-400"
-        >
-          <h2 className="text-4xl">My projects</h2>
-        </div>
-        <div className="mt-10 md:grid grid-flow-col gap-4 justify-center">
-          <Card title="GWENTcards" link="https://gwentcards.net">
-            GWENTcards is a site build with React &amp; Node that allows users to keep track of what cards they have
-            collected for The Witcher 3: Wild Hunt card mini game GWENT. It is also a PWA with full offline
-            functioniality. For this site I have build a custom authentication solution based on JWTs.
-          </Card>
-          <Card title="project argus" link="https://projectarg.us">
-            project argus is a site that allows you to track anything you want. At least, that is the plan. Currently it
-            allows tracking of movies &amp; tv shows using the tmdb API. It uses NextJS with prisma &amp; tailwind.
-          </Card>
-        </div>
-      </Section>
-      <div className="fixed bottom-5 left-2">
-        <Link href="#socials">
-          <a className="text-xs md:text-base text-white cursor-pointer hover:animate-bounce mb-5">
-            Socials &amp; <br></br> Languages
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="#FFFFFF"
-              className="h-6 w-6 md:h-12 md:w-12"
-            >
-              <path d="M0 0h24v24H0z" fill="none" />
-              <path d="M22 3H2C.9 3 0 3.9 0 5v14c0 1.1.9 2 2 2h20c1.1 0 1.99-.9 1.99-2L24 5c0-1.1-.9-2-2-2zm0 16H2V5h20v14zM21 6h-7v5h7V6zm-1 2l-2.5 1.75L15 8V7l2.5 1.75L20 7v1zM9 12c1.65 0 3-1.35 3-3s-1.35-3-3-3-3 1.35-3 3 1.35 3 3 3zm0-4c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1zm6 8.59c0-2.5-3.97-3.58-6-3.58s-6 1.08-6 3.58V18h12v-1.41zM5.48 16c.74-.5 2.22-1 3.52-1s2.77.49 3.52 1H5.48z" />
-            </svg>
-          </a>
-        </Link>
-        <Link href="#projects">
-          <a className="text-xs md:text-base text-white cursor-pointer hover:animate-bounce">
-            Projects
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="#FFFFFF"
-              className="h-6 w-6 md:h-12 md:w-12"
-            >
-              <path d="M0 0h24v24H0V0z" fill="none" />
-              <path d="M4 10.5c-.83 0-1.5.67-1.5 1.5s.67 1.5 1.5 1.5 1.5-.67 1.5-1.5-.67-1.5-1.5-1.5zm0-6c-.83 0-1.5.67-1.5 1.5S3.17 7.5 4 7.5 5.5 6.83 5.5 6 4.83 4.5 4 4.5zm0 12c-.83 0-1.5.68-1.5 1.5s.68 1.5 1.5 1.5 1.5-.68 1.5-1.5-.67-1.5-1.5-1.5zM7 19h14v-2H7v2zm0-6h14v-2H7v2zm0-8v2h14V5H7z" />
-            </svg>
-          </a>
-        </Link>
-      </div>
     </>
   );
 };
 
 const Section = ({ children }: any): JSX.Element => {
   return <section className="min-h-screen bg-dark-grey-500 p-10 md:p-5 text-center">{children}</section>;
-};
-
-const Card = ({ title, link, children }: any): JSX.Element => {
-  return (
-    <div className="flex flex-col gap-4 mb-2 bg-white/10 text-white p-4 max-w-lg">
-      <h3 className="text-2xl">{title}</h3>
-      <div>{children}</div>
-      <a href={link} target="_blank" rel="noreferrer" className="mt-auto block p-2 bg-white/20 hover:bg-white/30">
-        Visit site
-      </a>
-    </div>
-  );
 };
 
 export default Home;
