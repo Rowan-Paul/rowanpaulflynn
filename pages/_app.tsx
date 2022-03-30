4378a22f751657e08ebb11b57abc083caefb2884
Rowan-Paul <contact@rowanpaulflynn.com>
2022-03-30 15:20:33 +0200

    Fix typo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Rowan Paul Flynn</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content="Personal site for Rowan Paul Flynn, web developer in the Netherlands." />
+        <meta name="description" content="Personal site for Rowan Paul Flynn, web developer in The Netherlands." />
         <meta
           name="keywords"
           content="web developer, developer, web application,portfolio, projects, rowan-paul, rowan,flynn, rowan paul flynn, rowan-paul flynn, personal"
@@ -18,7 +18,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta property="og:title" content="The personal website for Rowan Paul Flynn" />
         <meta
           property="og:description"
-          content="Personal site for Rowan Paul Flynn, web developer in the Netherlands."
+          content="Personal site for Rowan Paul Flynn, web developer in The Netherlands."
         />
         <meta property="og:image" content="/icon.png" />
         <meta property="og:url" content="https://rowanpaulflynn.com" />
